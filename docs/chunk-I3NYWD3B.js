978242fbea63607e96c80a27b60df58e882bef13
unknown <lasleyendas1@gmail.com>
2025-07-30 19:18:11 -0400

    Actualización de código en Angular

--- docs/chunk-I3NYWD3B.js
+++
@@ -1,1 +0,0 @@
-import{X as a}from"./chunk-TPZDU73B.js";import"./chunk-FK42CRUA.js";export{a as DetalleComponent};
---
+++ docs/chunk-XDKL5COH.js
@@ -0,0 +1,1 @@
+import{Y as a}from"./chunk-G4KP7O4G.js";import"./chunk-FK42CRUA.js";export{a as DetalleComponent};
